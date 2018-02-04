86be2369954502cb67fd03295d68f36e438669f4
iamwijaya <31705906+iamwijaya@users.noreply.github.com>
2018-02-05 00:49:47 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# js
+Line Etot JS
+`Version : 0.2.4`
+
+# require node >= v8.x.x
+check your nodejs version
+`node -v`
+[upgrade nodejs](https://google.com/)
+
+
+How to run ?
+------
+- `git clone https://gitlab.com/teamanubot/LineEtotJS.git`
+- `cd LineEtotJS && npm install`
+- `npm start`
+
+New
+-------
+- Fixed Login (Masalah Error Login)
+
+Note
+-------
+- Jangan Perjual Belikan Script Ini!
+- Ini Alphat Lama Tapi Di Upgrade
+- Gunakan Script Ini Dengan Bijak!
+
+Still work :construction_worker:
+----
+**TODO** features
+- Implement All 
+- Improve logic
+
+Author
+------
+[@rivaimunte](http://line.me/ti/p/~kobe2k17)
---
+++ src/bot.js
@@ -0,0 +1,34 @@
+const LineConnect = require('./connect');
+let LINE = require('./main.js');
+console.info("\n\
+=========================================\n\
+BotName: LINE Etot JS\n\
+Version: 0.2.4\n\
+Terima Kasih Kepada @Alfathdirk @TCR_TEAM\n\
+=========================================\n\
+\nNOTE : Ini Adalah AlphatJS Lama Buatan @Alfathdirk @TCR_TEAM Dan Ini Telah Dikembangin Oleh @TAB_TEAM\nTolong Untuk Tidak Perjual-Belikan Script Ini!\n\
+=======nekopoi.band RUNING======");
+
+const auth = {
+	authToken: ' Toket Taro Sini Eh Token Maksudnya ',
+	certificate: ' Taro Cerf Disini ',
+}
+// let client =  new LineConnect(auth);
+let client =  new LineConnect();
+
+client.startx().then(async (res) => {
+	
+	while(true) {
+		try {
+			ops = await client.fetchOps(res.operation.revision);
+		} catch(error) {
+			console.log('error',error)
+		}
+		for (let op in ops) {
+			if(ops[op].revision.toString() != -1){
+				res.operation.revision = ops[op].revision;
+				LINE.poll(ops[op])
+			}
+		}
+	}
+});
